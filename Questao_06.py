8f1fd3c894c4e8ad740f8bcabf0b2b0e13d0b53d
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-06-03 19:11:10 -0300

    Update Questao_06.py

--- Questao_06.py
+++ Questao_06.py
@@ -1,8 +1,13 @@
 def escadinha(tam_lista, lista):
-    ordem = sorted(lista)
-    print(ordem)
-    for c in lista:
-        if c = lista[n]:
-            for n in lista:
-                
-
+    quantidade = 1
+    if tam_lista == 1 or tam_lista == 2:
+        quantidade = quantidade
+    else:
+        difAtual = lista[0] - lista[1]
+        for indice in range(1, tam_lista - 1):
+            difNova = lista[indice] - lista[indice + 1]
+            if difAtual != difNova:
+                difAtual = difNova
+                quantidade += 1
+    return quantidade
+print(escadinha(5,[11,-106,-223,-340,-457]))
